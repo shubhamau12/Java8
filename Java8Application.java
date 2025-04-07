201990b4970d4899db1af4b8da1300b66465f0a3
shubhamau12 <Shubham.jaiswal911@gmail.com>
2025-04-07 22:07:31 +0530

    Initial commit

---
+++ Java8Application.java
@@ -0,0 +1,14 @@
+package com.codewithshubham.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8Application.class, args);
+		//System.out.println("Hello");
+	}
+
+}
