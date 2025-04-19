ec4aaa8212b8133b4c1a0ad9e3e4c6f96336233b
shubhamau12 <shubham.jaiswal911@gmail.com>
2025-04-19 22:12:55 +0530

    updated

--- LongestString.java
+++ LongestString.java
@@ -8,8 +8,6 @@ public class LongestString {
 		List<String> strList=Arrays.asList("shubh","jaiswal","Manav","Charu","Tushar");
 		Optional<String> longestWord=strList.stream().reduce((word1,word2)-> word1.length()>word2.length() ? word1 : word2);
 		System.out.println("Longest word is:>"+longestWord.get());
-		
-
 	}
 
 }
